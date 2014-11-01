8027e147da79498cad4862186581892d6180e992
gvargas <vargas.gail@gmail.com>
2014-11-01 18:21:20 +0800

    Added sample search result Added bootstrap-table css and js updated general.html - added header to the page

--- src/main/java/com/acss/core/merchantupload/MerchantUploadController.java
+++ src/main/java/com/acss/core/merchantupload/MerchantUploadController.java
@@ -3,12 +3,16 @@ package com.acss.core.merchantupload;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import com.acss.core.support.web.MessageHelper;
 
 @Controller
 public class MerchantUploadController {
 	
 	@RequestMapping(value = "upload" , method = RequestMethod.POST)
-	public String upload(){
-		return "";
+	public String upload(RedirectAttributes ra){
+        MessageHelper.addSuccessAttribute(ra, "upload.success");
+		return "redirect:/";
 	}
 }
