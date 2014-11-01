59e931e5c6d8a6e0efb6f0d85be24e4df88ba09c
gvargas <vargas.gail@gmail.com>
2014-11-01 17:54:01 +0800

    Moved the addImage function to core.js Added add image function to merchant upload Added add image function to application detail

--- src/main/java/com/acss/core/merchantupload/MerchantUploadController.java
+++ src/main/java/com/acss/core/merchantupload/MerchantUploadController.java
@@ -5,13 +5,11 @@ import java.util.List;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
-import com.acss.core.signup.SignupForm;
 import com.acss.core.support.web.MessageHelper;
 
 @Controller
--- src/main/webapp/resources/js/core.js
+++ src/main/webapp/resources/js/core.js
@@ -8,3 +8,29 @@ String.prototype.format = String.prototype.f = function() {
     }
     return s;
 };
+
+
+function addImageFunction(){
+	/*! Customed function for dynamic addition of image with image type */
+	$("#addLink").click(function () {
+	    var id = ($('.additionalImage .row').length + 1).toString();
+	    $('#additionalImage').append('<h4></h4><div class="row" id="row-image_'+id+'">'
+	    		+'<div class="col-md-4"><div class="input-group svX3">'
+	    		+'<label for="inputImageType_'+id+'" style="width: 135px;" class="input-group-addon" id="imageType-addon_'+id+'">Image Type :&nbsp;</label>'
+	    		+'<select class="form-control" aria-describedby="imageType-addon_'+id+'" id="inputImageType_'+id+'" name="imageType_'+id+'">'
+	    		+ '<option value="0"></option><option value="1">Application Form</option><option value="2">ID Proof</option><option value="3">Proof of Billing</option><option value="4">Proof of Income</option>'
+	    		+'</select>'
+	    		+'</div>'
+	    		+'</div>'
+	    		+'<div class="col-md-8"><div class="input-group">'
+	    		+'<input type="file" class="form-control" id="image_'+id+'" name="image_'+id+'" aria-describedby="image_'+id+'-addon" />'
+	    		+'<span class="input-group-btn"><button type="button" class="btn btn-primary" onclick="resetForm(\'image_'+id+'\')">Clear</button></span></div></div></div>');
+
+	});
+
+	$("#removeLink").click(function () {
+		var id = ($('.additionalImage .row').length).toString();
+	    $("#additionalImage #row-image_"+ id).remove();
+	});
+	/*! Customed function for dynamic addition of image with image type */
+};
