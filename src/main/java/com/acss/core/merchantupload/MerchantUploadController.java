322c0ce03d2cd328290fc98427531e176d86fc35
gvargas <vargas.gail@gmail.com>
2014-11-01 18:57:59 +0800

    Added validation js file Updated the application detail screen 	-added image type dynamic files

--- src/main/java/com/acss/core/merchantupload/MerchantUploadController.java
+++ src/main/java/com/acss/core/merchantupload/MerchantUploadController.java
@@ -5,11 +5,13 @@ import java.util.List;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import com.acss.core.signup.SignupForm;
 import com.acss.core.support.web.MessageHelper;
 
 @Controller
@@ -48,5 +50,5 @@ public class MerchantUploadController {
 		appDetailsForm.setAppNo(appNo);
 		appDetailsForm.setImages(images);
 	}
-
+	
 }
