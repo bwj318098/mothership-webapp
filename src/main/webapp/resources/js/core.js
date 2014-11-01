cb2c8ffb6ace2fc68ffd9af76fd9b960c4cc0a2c
gvargas <vargas.gail@gmail.com>
2014-11-01 18:07:07 +0800

    updated the grid definition for mobile view

--- src/main/webapp/resources/js/core.js
+++ src/main/webapp/resources/js/core.js
@@ -15,14 +15,14 @@ function addImageFunction(){
 	$("#addLink").click(function () {
 	    var id = ($('.additionalImage .row').length + 1).toString();
 	    $('#additionalImage').append('<h4></h4><div class="row" id="row-image_'+id+'">'
-	    		+'<div class="col-md-4"><div class="input-group svX3">'
+	    		+'<div class="col-md-4 col-xs-6"><div class="input-group svX3">'
 	    		+'<label for="inputImageType_'+id+'" style="width: 135px;" class="input-group-addon" id="imageType-addon_'+id+'">Image Type :&nbsp;</label>'
 	    		+'<select class="form-control" aria-describedby="imageType-addon_'+id+'" id="inputImageType_'+id+'" name="imageType_'+id+'">'
 	    		+ '<option value="0"></option><option value="1">Application Form</option><option value="2">ID Proof</option><option value="3">Proof of Billing</option><option value="4">Proof of Income</option>'
 	    		+'</select>'
 	    		+'</div>'
 	    		+'</div>'
-	    		+'<div class="col-md-8"><div class="input-group">'
+	    		+'<div class="col-md-8 col-xs-6"><div class="input-group">'
 	    		+'<input type="file" class="form-control" id="image_'+id+'" name="image_'+id+'" aria-describedby="image_'+id+'-addon" />'
 	    		+'<span class="input-group-btn"><button type="button" class="btn btn-primary" onclick="resetForm(\'image_'+id+'\')">Clear</button></span></div></div></div>');
 
