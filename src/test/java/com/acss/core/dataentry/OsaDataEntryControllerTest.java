ad8e9124c55f9d443b0d377196672390fbc63474
gvargas <vargas.gail@gmail.com>
2015-11-25 14:05:31 +0800

    Removed Unused imports

--- src/test/java/com/acss/core/dataentry/OsaDataEntryControllerTest.java
+++ src/test/java/com/acss/core/dataentry/OsaDataEntryControllerTest.java
@@ -1,9 +1,16 @@
 package com.acss.core.dataentry;
 
+import static com.acss.core.RequestParamUtils.addParams;
+import static org.hamcrest.CoreMatchers.anyOf;
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.CoreMatchers.notNullValue;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
 import java.math.BigDecimal;
 import java.nio.charset.Charset;
-
-import javax.annotation.Resources;
 
 import org.junit.Before;
 import org.junit.Test;
@@ -14,23 +21,16 @@ import org.mockito.MockitoAnnotations;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.ContextConfiguration;
-import org.springframework.test.context.TestContext;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.web.AnnotationConfigWebContextLoader;
 import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.ResultActions;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
-import org.springframework.validation.BindException;
-import org.springframework.validation.BindingResult;
-import org.springframework.validation.FieldError;
-import org.springframework.web.context.WebApplicationContext;
 import org.springframework.web.servlet.View;
 
 import com.acss.core.config.ApplicationConfig;
 import com.acss.core.config.SecurityConfig;
-import com.acss.core.config.WebAppInitializer;
 import com.acss.core.config.WebMvcConfig;
 import com.acss.core.model.builder.AddressFieldBuilder;
 import com.acss.core.model.builder.DataEntryDTOBuilder;
@@ -41,15 +41,7 @@ import com.acss.core.model.builder.ReferenceDataBuilder;
 import com.acss.core.model.builder.StoreInformationBuilder;
 import com.acss.core.model.dataentry.DataEntryDTO;
 import com.acss.kaizen.jooq.poc.configuration.PersistenceContext;
-import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
-
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
-import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.*;
-import static org.junit.Assert.*;
-import static org.hamcrest.CoreMatchers.*;
-import static com.acss.core.RequestParamUtils.*;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @WebAppConfiguration
